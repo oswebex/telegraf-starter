47059bf7dc3330bfe5190ce3ff62b04eb815677b
Oscar Sultanov <oscars@live.ru>
2021-01-26 21:34:25 +0500

    Add fetchTimeout

--- src/telegraf.ts
+++ src/telegraf.ts
@@ -1,8 +1,21 @@
 import { Telegraf as TelegrafSource, Context } from "telegraf";
+import { promisify } from 'util'
 
+const delay = promisify(setTimeout)
+// @ts-expect-error
 export class Telegraf<TContext extends Context> extends TelegrafSource<TContext> {
   configure(callback: (this: this, app: this) => void): this {
     callback.call(this, this);
     return this;
+  }
+  
+  handleUpdates(updates: any) {
+    // @ts-expect-error
+    if (this.options.fetchTimeout) {
+      // @ts-expect-error
+      return Promise.race([super.handleUpdates(updates), delay(this.options.fetchTimeout)])
+    }
+    // @ts-expect-error
+    return super.handleUpdates(updates)
   }
 }
