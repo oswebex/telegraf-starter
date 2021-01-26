ed2fc0bd90adeddb1e532aaec656d7555cfed6b8
Oscar Sultanov <oscars@live.ru>
2021-01-26 21:43:49 +0500

    Fix launcher for support skip updates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import bot from "./bot";
 
-bot.launch()
-  .then(() => console.info('The bot is launched'))
-  .catch(err => console.error(err))
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
+(async function() {
+  try {
+    if (process.env.BOT_SKIP_UPDATES) {
+      void await bot.telegram.callApi('getUpdates', { offset: -1 })
+    }
+    void await bot.launch()
+    console.info('The bot is launched')
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+  } catch (err) {
+    console.error(err)
+  }
+}())
 
 process.on('unhandledRejection', (reason, p) => console.error('%j %j', p, reason))
