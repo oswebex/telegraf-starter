788f8834e33f0784ac38ace13262e08db0ce8b11
Oscar Sultanov <oscars@live.ru>
2021-01-27 13:50:57 +0500

    Misc logmiddle

--- src/middlewares/log.middle.ts
+++ src/middlewares/log.middle.ts
@@ -1,11 +1,11 @@
 import { MyContext } from "../declarations";
 
 export default () => async function (ctx: MyContext, next: any) {
-  const start = +new Date()
+  const start = new Date()
 
   await next()
 
-  const ms = +new Date() - start
+  const ms = +new Date() - +start
 
-  console.log('%s ms', ms)
+  console.info('[%s][%s/%s][%s][%sms]', start.toLocaleTimeString(), ctx.from?.id, ctx.from?.username, ctx.updateType,  ms)
 }
